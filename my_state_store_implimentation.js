09e5c71993be98011a9e0f7bc2b68ed85af61c6f
Matthew Miller <mmlak_0109@yahoo.com>
2021-02-15 22:36:55 +0900

    Initial commit for this file: Contains CreateStore function and "todo"  action creator function CreateStore  contains "state" and "listeners" variables, "getState", "subscribe", and "dispatch" functions, and returns those three functions

---
+++ my_state_store_implimentation.js
@@ -0,0 +1,54 @@
+// Library code
+function createStore(reducer) {
+    // The store should have 4 parts
+    // 1. The state
+    // 2. Get the state
+    // 3. Listen to changes on the state
+    // 4. Update the state
+
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    };
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach((listener) => listener());
+    };
+
+    return {
+        getState,
+        subscribe,
+        dispatch,
+    };
+}
+
+// App code
+function todos(state = [], action) {
+    if (action.type === 'ADD_TODO') {
+        return state.concat([action.todo])
+    }
+
+    return state
+};
+
+const store = createStore(todos);
+store.subscribe(() => {
+    console.log('The new state is: ', store.getState())
+});
+
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false
+    }
+});

1dffcaf3213741c9e4aaa903ea9e9a9eea42fa02
Matthew Miller <mmlak_0109@yahoo.com>
2021-02-16 22:41:25 +0900

    Reducer and Dispatch Updates:
    Changed action types from strings to variables that reference stings,
    this way errors will be thrown if a misspelling occurs.
    
    Created "action creator" functions to use with the dispatch function.
    This reduces code dupplication and human error.

--- my_state_store_implimentation.js
+++ my_state_store_implimentation.js
@@ -30,14 +30,56 @@ function createStore(reducer) {
     };
 }
 
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
+function addTodoAction(todo) {
+    return {
+        type: ADD_TODO,
+        todo
+    }
+};
+
+function removeTodoAction(id) {
+    return {
+        type: REMOVE_TODO,
+        id
+    }
+};
+
+function toggleTodoAction(id) {
+    return {
+        type: TOGGLE_TODO,
+        id
+    }
+};
+
+function addGoalAction(goal) {
+    return {
+        type: ADD_GOAL,
+        goal
+    }
+};
+
+function removeGoalAction(id) {
+    return {
+        type: REMOVE_GOAL,
+        id
+    }
+};
+
+
 // App code
 function todos(state = [], action) {
     switch (action.type) {
-        case 'ADD_TODO':
+        case ADD_TODO:
             return state.concat([action.todo])
-        case 'REMOVE_TODO':
+        case REMOVE_TODO:
             return state.filter((todo) => todo.id !== action.id)
-        case 'TOGGLE_TODO':
+        case TOGGLE_TODO:
             return state.map((todo) =>
                 todo.id !== action.id
                 ? todo
@@ -49,9 +91,9 @@ function todos(state = [], action) {
 
 function goals(state = [], action) {
     switch(action.type) {
-        case 'ADD_GOAL':
+        case ADD_GOAL:
             return state.concat([action.goal])
-        case 'REMOVE_GOAL':
+        case REMOVE_GOAL:
             return state.filter((goal) => goal.id !== action.id)
         default:
             return state
@@ -73,11 +115,18 @@ store.subscribe(() => {
     console.log('The new state is: ', store.getState())
 });
 
-store.dispatch({
-    type: 'ADD_TODO',
-    todo: {
+// store.dispatch({
+//     type: ADD_TODO,
+//     todo: {
+//         id: 0,
+//         name: 'Learn Redux',
+//         complete: false
+//     }
+// });
+
+store.dispatch(addTodoAction({
         id: 0,
         name: 'Learn Redux',
         complete: false
     }
-});
+));

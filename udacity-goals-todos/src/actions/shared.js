e46719a65f144de6077d2eb772cc793f0de37ed5
Matthew Miller <mmlak_0109@yahoo.com>
2021-02-25 23:34:27 +0900

    Oraganize files and copy over relevant data
    - Organize folders in "rails-style".
    - Separate variables and functions  into separate files

---
+++ udacity-goals-todos/src/actions/shared.js
@@ -0,0 +1,26 @@
+import API from 'goals-todos-api';
+
+export const RECIEVE_DATA = "RECIEVE_DATA";
+
+function recieveData(todos, goals) {
+    return {
+        type: RECIEVE_DATA,
+        todos,
+        goals
+    }
+}
+
+export function handleInitialData() {
+    return (dispatch) => {
+        return Promise.all([
+            API.fetchTodos(),
+            API.fetchGoals()
+        ]).then(([ todos, goals ]) => {
+            dispatch(recieveData(
+                todos,
+                goals
+            ))
+        })
+    }
+}
+
